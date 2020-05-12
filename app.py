ed7a7fb3ccfc5f5c72c2cbe06720cd6f6b711a97
osagie eboigbe <osagie_eboigbe@yahoo.com>
2020-05-12 07:37:41 +0100

    add changes

--- app.py
+++ app.py
@@ -2,16 +2,21 @@ from flask import Flask,render_template,request
 import pandas as pd
 import numpy as np
 import pickle
+
 app=Flask(__name__)
 model=pickle.load(open("cars_useds_predictions.pkl",'rb'))
+
+
 @app.route('/')
 def home():
     return render_template("home.html")
+
 @app.route('/predict',methods=['POST'])
 def predict():
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
+    prediction = round(prediction[0], 2)
     return render_template("home.html", prediction_text='Price should be $ {}'.format(prediction))
 
 
